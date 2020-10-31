aeea0f6a692c555834c3c49cf3497baf3cefc646
99002512 <71485083+99002512@users.noreply.github.com>
2020-10-31 17:15:36 +0530

    Update Second.js

--- Second/Second.js
+++ Second/Second.js
@@ -1,8 +1,7 @@
 describe('Suite  to test classses',()=> {
+   
     let e=null;
     
-
-
     it('It should set ',()=>{
        const e = new employee("Koushik","af@g")
        const b=emp.validateName(e);
@@ -15,12 +14,6 @@ describe('Suite  to test classses',()=> {
      })
 
 
-     it('should set Phone',()=>{
-        const e = new employee("Koushik","k","8056272472",8056272472)
-        const check2=emp.validatePhone(e);
-         expect(true).toBe(check2)
-     })
-     
     
 })
 
